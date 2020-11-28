02a2453812756d12751e21771f85d3b69e9ea023
maks <root@localhosst>
2020-11-28 16:31:08 +0200

    Initial Commit

---
+++ RoadSignDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RoadSignDetector
+//
+//  Created by Макс Пайдич on 28.11.2020.
+//  Copyright © 2020 Макс Пайдич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
